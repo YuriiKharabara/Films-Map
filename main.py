3ab5d269ff775df80b18619ee57c0287286fa8a3
YuriiKhr <yurii.kharabara@ucu.edu.ua>
2022-02-05 22:50:50 +0200

    list of tuples with films properties

--- main.py
+++ main.py
@@ -7,8 +7,27 @@ def info_get(path_to_file):
     return data
 
 
+def convert_info(data):
+    i=0
+    while not data[i].startswith('='):
+        i+=1
+    i+=1
+    films=[]
+    for j in range(i, len(data)-1):    
+        name=data[j].split('(')[0]
+        year=data[j].split('(')[1][:4]
+        row_for_location=data[j].split('\t')
+        amount=row_for_location.count('')
+        for i in range(amount):
+            row_for_location.remove('')
+        locations=row_for_location[1:]
+        location=' '.join(locations)
+        location=location[:-1]
+        films.append((name, year, location))
+    return films
 
 
 if __name__ == '__main__':
     data=info_get('locations.list')
-    print(data)
+    films=convert_info(data)
+    print(films)
